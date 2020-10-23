327bc0a489979a65c3e0d4ae59fff9bd06acc152
arnautovd <arnautov.dmitry2105@yandex.ru>
2020-10-23 22:35:29 +0300

    new func is here new func for working with file adding new file for reading data from last one replace the calling of func adding new line to file adding new module adding functions in a new func import and calling the function creating a new func in the first file

--- first_module.py
+++ first_module.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 
 def get_sum(a, b):
@@ -7,3 +8,7 @@ def get_sum(a, b):
 
 def get_specific_calc(a, b, c):
     return math.pow(get_sum(a, b), c)
+
+
+def get_random_num():
+    return random.choice(range(1, 100))
